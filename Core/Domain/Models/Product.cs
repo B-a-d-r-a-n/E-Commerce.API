63a3a338ace42b9cf05fa6db3df0c85b5b2f4377
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-04-07 17:02:41 +0200

    Product Module Entities

---
+++ Core/Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Domain.Models
+{
+    public class Product :BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public ProductBrand ProductBrand { get; set; } //reference nav property for brand
+        public int BrandId { get; set; }//fk
+        public ProductType ProductType { get; set; } //reference navigation property for type
+        public int TypeId { get; set; }//fk
+
+    }
+}

d3d7468a2a6f5c5153e85f186421bf0c798ae21c
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-04-14 23:20:12 +0200

    Specifications in action

--- Core/Domain/Contracts/IGenericRepository.cs
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -7,7 +7,9 @@ namespace Domain.Contracts
     public interface IGenericRepository<TEntity,TKey> where TEntity : BaseEntity<TKey>
     {
         Task<TEntity?> GetAsync(TKey id);
+        Task<TEntity?> GetAsync(ISpecifications<TEntity> specifications);
         Task<IEnumerable<TEntity>> GetAllAsync(bool trackChanges = false);
+        Task<IEnumerable<TEntity>> GetAllAsync(ISpecifications<TEntity> specifications);
         void Delete(TEntity entity);
         void Add(TEntity entity);
         void Update(TEntity entity);
--- Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using Domain.Contracts;
 using Persistence.Data;
 
@@ -25,7 +21,14 @@ namespace Persistence.Repositories
 
 
         public async Task<TEntity?> GetAsync(TKey id) => await context.Set<TEntity>().FindAsync(id);
-
         public void Update(TEntity entity) => context.Set<TEntity>().Update(entity);
+
+        public async Task<TEntity?> GetAsync(ISpecifications<TEntity> specifications)
+            => await SpecificationsEvaluator.CreateQuery(context.Set<TEntity>(), specifications)
+                .FirstOrDefaultAsync();
+        public async Task<IEnumerable<TEntity>> GetAllAsync(ISpecifications<TEntity> specifications) 
+            => await SpecificationsEvaluator.CreateQuery(context.Set<TEntity>(), specifications)
+                 .ToListAsync();
+
     }
 }

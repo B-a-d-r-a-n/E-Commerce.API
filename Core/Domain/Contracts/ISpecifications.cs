d4d87e070d865820e973065bfc0b8a1155ffd71a
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-04-14 22:35:36 +0200

    Specifications

---
+++ Core/Domain/Contracts/ISpecifications.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Linq.Expressions;
+
+namespace Domain.Contracts
+{
+    public interface ISpecifications<T> where T : class
+    {
+        Expression<Func<T,bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> IncludeExpressions { get; }
+    }
+}
+// where => Expression<Func<T,bool>> Criteria
+//include=> List<expression<func<T,object>>>
+//select => criteria expression<func<T,Tresult>>
---
+++ Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Linq.Expressions;
+
+namespace Services.Specifications
+{
+    internal class ProductWithBrandAndTypeSpecifications : BaseSpecifications<Product>
+    {
+        // use the constructor to create query to get product by id
+        public ProductWithBrandAndTypeSpecifications(int id)
+            : base(product => product.Id == id)
+        {
+            //add includes
+            AddInclude(p=> p.ProductBrand);
+            AddInclude(p=> p.ProductType);
+        }
+        // use the constructor to create query to get all product
+        public ProductWithBrandAndTypeSpecifications()
+            :base(null)
+        {
+            AddInclude(p => p.ProductBrand);
+            AddInclude(p => p.ProductType);
+        }
+    }
+}

6a5d3fa26668dac78300547e610cac21c431ac23
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-04-19 04:15:54 +0200

    Basket Service

--- Core/Domain/Models/Basket/BasketItem.cs
+++ Core/Domain/Models/Basket/BasketItem.cs
@@ -3,10 +3,10 @@ namespace Domain.Models.Basket
 {
     public class BasketItem
     {
-        public string Id { get; set; }
+        public int Id { get; set; } //product id == int
         public string ProductName { get; set; } = default!;
         public string PictureUrl { get; set; } = default!;
         public decimal Price { get; set; }
-        public int Quanityty { get; set; }
+        public int Quantity { get; set; }
     }
 }
---
+++ Shared/DataTransferObjects/Basket/BasketDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.DataTransferObjects.Basket
+{
+    public record BasketDTO
+    {
+        public string Id { get; init; }
+        public ICollection<BasketItemDTO> BasketItems { get; init; } = [];
+    }
+
+    }

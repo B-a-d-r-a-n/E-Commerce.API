c06e4143b8ce4a0c4580db43cf7db8cb9ab585a9
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-05-09 04:54:20 +0300

    Order Module Entities

---
+++ Core/Domain/Models/OrderModels/Order.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Domain.Models.OrderModels
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public Order()
+        {
+            
+        }
+        public Order(string userEmail,
+            IEnumerable<OrderItem> items,
+            OrderAddress address,
+            DeliveryMethod deliveryMethod,
+            decimal subtotal)
+        {
+            UserEmail = userEmail;
+        
+            Items = items;
+            Address = address;
+            DeliveryMethod = deliveryMethod;
+            //PaymentIntentId = paymentIntentId;
+            Subtotal = subtotal;
+        }
+
+        // Id guid
+        public string UserEmail { get; set; } = default!;
+        public DateTimeOffset Date { get; set; } = DateTimeOffset.Now;
+        public IEnumerable<OrderItem> Items { get; set; } = [];
+        public OrderAddress Address { get; set; } = default!;
+        public DeliveryMethod DeliveryMethod { get; set; } = default!;
+        public int DeliveryMethodId { get; set; }
+        public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Pending; // default value
+        public string PaymentIntentId { get; set; } = string.Empty; // Stripe payment intent id
+        public decimal Subtotal { get; set; } // subtotal of the order
+
+    }
+}
---
+++ Infrastructure/Presentation/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+
+namespace Presentation.Controllers
+{
+    [Authorize]
+    public class OrderController
+        :APIController
+    {
+        /// create(address,basketId,deliveryMethodId) => *return* OrderResponse
+        /// GetDeliveryMethods
+        /// 
+    }
+}

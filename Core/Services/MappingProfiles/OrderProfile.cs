25d2495cd8643f2b225846eee332a8772144ba2c
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-05-10 00:22:47 +0300

    Order Profile

---
+++ Core/Services/MappingProfiles/OrderProfile.cs
@@ -0,0 +1,36 @@
+﻿
+using Domain.Models.OrderModels;
+using Microsoft.Extensions.Configuration;
+using Shared.Orders;
+
+namespace Services.MappingProfiles
+{
+    internal class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<OrderAddress,AddressDTO>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDTO>()
+                .ForMember(d => d.ProductName, option =>
+                    option.MapFrom(s => s.Product.ProductName))
+                .ForMember(d => d.PictureUrl, option =>
+                option.MapFrom<OrderItemPictureUrlResolver>());
+            CreateMap<Order, OrderResponse>()
+                .ForMember(d => d.DeliveryMethod, options =>
+                options.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.Total, options =>
+                options.MapFrom(s => s.DeliveryMethod.Price + s.Subtotal));
+
+
+        }
+    }
+    internal class OrderItemPictureUrlResolver(IConfiguration configuration)
+        : IValueResolver<OrderItem, OrderItemDTO, string>
+    {
+        public string Resolve(OrderItem source, OrderItemDTO destination, string destMember, ResolutionContext context)
+        {
+            return string.IsNullOrWhiteSpace(source.Product.PictureUrl) ? string.Empty 
+                : $"{configuration["BaseUrl"]}{source.Product.PictureUrl}";
+        }
+    }
+}
